5c25013f44fcfdac7f785a8f98e85a027e15d7f9
aaronjoju07 <aaronjoju07@gmail.com>
2023-11-14 10:36:41 +0530

    .

---
+++ Bicycle.java
@@ -0,0 +1,15 @@
+public class Bicycle {
+    // the bicycle class fields
+    public int cadence;
+    public int gear;
+    public int speed;
+
+    //  thr bicycle has a constructor 
+    public Bicycle(int startCadence, int startGear, int startSpeed){
+        gear  = startGear;
+        cadence = startCadence;
+        speed = startSpeed;
+    }
+    // the bucycle class has four methods
+    
+}
