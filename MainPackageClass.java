f481cb6bf2be36e35bc8843884425aa3833742b3
aaronjoju07 <aaronjoju07@gmail.com>
2023-12-12 00:11:37 +0530

    pract

---
+++ MainPackageClass.java
@@ -0,0 +1,7 @@
+import Packages.*;;
+public class MainPackageClass {
+    public static void main(String[] args) {
+        MyFirstPackage mfp = new MyFirstPackage();
+        mfp.display();
+    }
+}
---
+++ Packages/MyFirstPackage.java
@@ -0,0 +1,7 @@
+package Packages;
+
+public class MyFirstPackage {
+    public void display(){
+        System.out.println("Display my First Package");
+    }
+}
